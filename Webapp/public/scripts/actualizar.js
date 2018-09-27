cfd16654d5c5d77faed79eb62f579d5ec0e470fd
Jean Pierre <pierre1326@hotmail.com>
2018-09-27 17:15:01 -0600

    Primeras 9 sentencias probadas

--- Webapp/public/scripts/actualizar.js
+++ Webapp/public/scripts/actualizar.js
@@ -23,7 +23,6 @@ function actualizar() {
     }
   });
   var envio = { "where" : JSON.stringify(filtros), "set" : JSON.stringify(valores) };
-  console.log(envio);
   $.post(ip + "/update/" + tablaActual, envio, function(info){
     alert(info);
   });
